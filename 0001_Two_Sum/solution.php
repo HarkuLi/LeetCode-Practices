47754705aa507ff374e2c4e1a1d603c792746091
HarkuLi <harku85697708@hotmail.com>
2021-10-17 22:20:45 +0800

    Improve php solution for problem 1

--- 0001_Two_Sum/solution.php
+++ 0001_Two_Sum/solution.php
@@ -5,51 +5,26 @@
 
 class Solution {
     /**
-     * @param Integer[] $nums
-     * @param Integer $target
-     * @return Integer[]
+     * @param int[] $nums
+     * @param int $target
+     * @return int[]
      */
     function twoSum($nums, $target) {
-        $indexMap = $this->getIndexMap($nums);
-        
-        $numCount = count($nums);
-        for ($idx = 0; $idx < $numCount; ++$idx) {
-            $num = $nums[$idx];
-            $expectNum = $target - $num;
-            
-            if ($expectNum === $num) {
-                if (count($indexMap[$num]) >= 2) {
-                    // We can return directly because there is exactly one solution.
-                    return $indexMap[$num];
-                }
-                
-                continue;
-            }
-            
-            if (array_key_exists($expectNum, $indexMap)) {
-                return [$idx, $indexMap[$expectNum][0]];
-            }
-        }
-        
-        return [];
-    }
-
-    /**
-     * format: [number -> [position1, position2, ...]]
-     *
-     * @return array
-     */
-    private function getIndexMap($nums) {
+        /**
+         * @var array<int, int> Number to index map.
+         */
         $indexMap = [];
-        $numCount = count($nums);
-        for ($idx = 0; $idx < $numCount; ++$idx) {
-            $num = $nums[$idx];
-            if (array_key_exists($num, $indexMap)) {
-                $indexMap[$num][] = $idx;
-            } else {
-                $indexMap[$num] = [$idx];
+
+        foreach ($nums as $index => $num) {
+            $expectNum = $target - $num;
+
+            if (array_key_exists($expectNum, $indexMap)) {
+                return [$indexMap[$expectNum], $index];
             }
+
+            $indexMap[$num] = $index;
         }
-        return $indexMap;
+
+        return [];
     }
 }
